ab0837a9bf740cc415626753019aa1bdfa601bfe
Alexandra Savramis <alexandra.savramis@gmail.com>
2019-05-23 00:05:30 -0700

    Add dummy server and request

---
+++ dummy_server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+app.use(cors())
+
+app.post('/submit', function(req, res) {
+    let email = req.body.email;
+    
+    console.log(JSON.stringify(req.body))
+    console.log(`my email is: ${email}`)
+
+    res.send('Success!')
+});
+
+app.listen(3001, () => {
+ console.log("Server running on port 3001");
+});
--- mobile-dialog/src/App.js
+++ mobile-dialog/src/App.js
@@ -10,12 +10,26 @@ class App extends Component {
     }
   }
   
+  async submit(state) {
+    await fetch('http://localhost:3001/submit', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(state)
+    })
+    
+    this.setState({showModal: false})
+  }
+  
   render() {
     return (
       <div className="App">
         {this.state.showModal ?
           <MobileDeviceSettingsModal
             dismissModal={() => this.setState({showModal: false})}
+            onSave={this.submit.bind(this)}
           />
           :
           <button onClick={() => this.setState({showModal: !this.state.showModal})}>Show Modal</button>
